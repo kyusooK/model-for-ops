747e8a6d55693cd3a3a5468938e4dd24701a5a42
박용주 <apexacme@gmail.com>
2023-11-03 09:10:59 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,17 +3,19 @@ sudo apt-get install net-tools
 sudo apt install iputils-ping
 pip install httpie
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+minikube start && minikube addons enable metrics-server
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "../awscliv2.zip"
-unzip -o ../awscliv2.zip -d ../
-sudo .././aws/install
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "../awscliv2.zip"
+#unzip -o ../awscliv2.zip -d ../
+#sudo .././aws/install
 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 
+#curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+#sudo mv /tmp/eksctl /usr/local/bin
+
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 
 
 # If you are using GCP, Uncomment # to install gcloud tool.
 # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
