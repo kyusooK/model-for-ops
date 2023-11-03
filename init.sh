de37bdd8ca5a7da503d66919630c12eeb7bec614
박용주 <apexacme@gmail.com>
2023-11-03 09:15:58 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@ sudo apt-get install net-tools
 sudo apt install iputils-ping
 pip install httpie
 
-minikube start && minikube addons enable metrics-server
+minikube start
+minikube addons enable metrics-server
 
 #curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 #sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
